f95fd32b6485e0f053aaec9752176857f970b3c9
Ilyass Taouil <itaouil95@gmail.com>
2017-06-10 19:24:48 +0200

    Basic structure, css, js and html.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  // Paddle references
+  var paddleA = $('#paddleA');
+  var paddleB = $('#paddleB')
+
+  // CSS postion offsetting
+  paddleA.css('top', '60px');
+  paddleB.css('top', '20px');
+
+});
